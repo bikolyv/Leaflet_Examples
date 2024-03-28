2138a6ff68825c8916157e56a5a2c58353e598b9
bikolyv <stu2201262036@uni-plovdiv.bg>
2024-03-28 16:37:52 +0200

    Layer Groups and Layers Control

--- init_project/main.js
+++ init_project/main.js
@@ -1,11 +1,46 @@
 var geojson;
 
-var map = L.map('map-container').setView([37.8, -96], 4);
+var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
+    denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
+    aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
+    golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
+var cities = L.layerGroup([littleton, denver, aurora, golden]);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var crownHill = L.marker([39.75, -105.09]).bindPopup('This is Crown Hill Park.'),
+    rubyHill = L.marker([39.68, -105.00]).bindPopup('This is Ruby Hill Park.');
+var parks = L.layerGroup([crownHill, rubyHill]);
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    attribution: '© OpenStreetMap'
+});
+var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
+});
+var openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'Map data: © OpenStreetMap contributors, SRTM | Map style: © OpenTopoMap (CC-BY-SA)'
+});
+
+var map = L.map('map-container', {
+    center: [39.73, -104.99],
+    zoom: 10,
+    layers: [osm, cities]
+});
+
+var baseMaps = {
+    "OpenStreetMap": osm,
+    "<span style='color: red'>OpenStreetMap.HOT</span>": osmHOT
+};
+
+var overlayMaps = {
+    "Cities": cities
+};
+
+var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+layerControl.addBaseLayer(openTopoMap, "OpenTopoMap");
+layerControl.addOverlay(parks, "Parks");
 
 function getColor(density) {
     return density > 1000 ? '#800026' :
